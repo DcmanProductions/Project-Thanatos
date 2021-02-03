9fcdb3298add5596e4a66392e1f8878beb706322
Drew Chase <dcmanproductions@gmail.com>
2021-02-03 16:12:25 -0500

    Updated Engine Version to 4.26

---
+++ Source/ProjectThanatos/ProjectThanatos.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectThanatos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectThanatos, "ProjectThanatos" );
